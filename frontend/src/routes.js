a658c1720106b32ffc20c018e00250a982951e08
unknown <wallacecardosogomes@gmail.com>
2020-04-04 18:37:29 -0300

    Criando página cadastrar caso

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Logon from './pages/Logon';
 import Resgister from './pages/Register';
 import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 export default function Routes() {
     return (
@@ -15,6 +16,7 @@ export default function Routes() {
                 <Route path= "/" exact component={Logon} />
                 <Route path= "/register" component={Resgister} />
                 <Route path= "/profile" component={Profile} />
+                <Route path= "/incidents/new" component={NewIncident} />
             </Switch>
         </BrowserRouter>
     );
