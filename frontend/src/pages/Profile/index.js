8c3dcc0d33dadaf8fb683d7689be67276c5dd40c
unknown <wallacecardosogomes@gmail.com>
2020-04-04 20:54:05 -0300

    Finalizando crud básico front-end

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -3,7 +3,7 @@
 */
 
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -19,6 +19,8 @@ export default function Profile() {
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
 
+    const history = useHistory();
+
     //useEffect((função que será executada) => {}, [quando executar(caso vazio, excuta ao render)])
     useEffect(() => {
         api.get('profile', {
@@ -30,6 +32,27 @@ export default function Profile() {
         });
     }, [ongId]);
 
+    async function handleDeleteIncident (id) {
+        try {
+            await api.delete(`incidents/${id}`, {
+                headers: {
+                    Authorization: ongId,
+                }
+            });
+            //mantêm no state apenas os incidents que o id for diferente do deletado
+            //poderia ser feito utilizando o useEffect também
+            setIncidents(incidents.filter(incident => incident.id !== id));
+        }catch (err) {
+            alert('Falha ao tentar deletar caso')
+        }
+    }
+
+    function handleLogout() {
+        localStorage.clear();
+
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -37,7 +60,7 @@ export default function Profile() {
                 <span>Bem vinda, {ongName}</span>
 
                 <Link className="button" to="incidents/new">Cadastrar novo caso</Link>
-                <button type="button" >
+                <button onClick={handleLogout} type="button" >
                     <FiPower size={18} color="#E02041" />
                 </button>
             </header>
@@ -54,7 +77,7 @@ export default function Profile() {
                         <strong>Valor:</strong>
                         <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
 
-                        <button type="button" >
+                        <button onClick={() => handleDeleteIncident(incident.id)} type="button" >
                             <FiTrash2 size={20} color="#a8a8b3" />
                         </button>
                     </li>
