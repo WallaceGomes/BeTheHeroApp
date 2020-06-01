32330dc0a3e2e161b9eff45a5b6dcbca41e7b151
unknown <wallacecardosogomes@gmail.com>
2020-06-01 18:35:05 -0300

    chore: details page with styles, navigation and linking

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,8 +1,66 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { View, Image, Text, TouchableOpacity, Linking } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import * as MailCompose from 'expo-mail-composer';
+
+import logoImg from '../../assets/logo.png';
+import styles from './styles';
 
 export default function Detail() {
-    return(
-        <View/>
-    );
+  const navigation = useNavigation();
+
+  const message = 'Ola ONG, estou entrando em contato sobre o caso tal e tals';
+
+  function navigateBack() {
+    navigation.goBack();
+  }
+
+  function sendMail() {
+    MailCompose.composeAsync({
+      subject: 'Herós do caso: Cadelinha atropelada',
+      recipients: ['wallacecardosogomes@gmail.com'],
+      body: message,
+    });
+  }
+
+  function sendWhatsApp() {
+    Linking.openURL(`whatsapp://send?phone=021975678836&text=${message}`);
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <TouchableOpacity onPress={navigateBack}>
+          <Feather name="arrow-left" size={28} color="#E82041" />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.incident}>
+        <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
+        <Text style={styles.incidentValue}>APAD</Text>
+        <Text style={styles.incidentProperty}>CASO:</Text>
+        <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+        <Text style={styles.incidentProperty}>VALOR:</Text>
+        <Text style={styles.incidentValue}>R$ 120,00</Text>
+      </View>
+      <View style={styles.contactBox}>
+        <Text style={styles.heroTitle}>Salve o dia!</Text>
+        <Text style={styles.heroTitle}>Seja o herói desse caso.</Text>
+
+        <Text style={styles.description}>Entre em contato:</Text>
+
+        <View style={styles.actions}>
+          <TouchableOpacity style={styles.action} onPress={sendWhatsApp}>
+            <Text style={styles.actionText}>WhatsApp</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.action} onPress={sendMail}>
+            <Text style={styles.actionText}>E-mail</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
 }
